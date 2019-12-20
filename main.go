0c50f2be64a3105f7a972d21503a827b4afbc2a1
Kevin Travers <ktravers@cbs.com>
2019-12-20 15:16:51 -0500

    Updating service

--- main.go
+++ main.go
@@ -1,34 +1,9 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"github.com/CBSktravers/hooli/server"
+	"fmt"
 )
-
-var (
-	HooliCertFile    = "./certs/local.localhost.cert" //os.Getenv("Hooli_CERT_FILE")
-	HooliKeyFile     = "./certs/local.localhost.key"  //os.Getenv("Hooli_KEY_FILE")
-	HooliServiceAddr = ":8080"                        //os.Getenv("Hooli_SERVICE_ADDR")
-)
-
-const message = "Hello Wolrd"
-
-//const HooliServiceAddr = ":8080"
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(message))
-	})
-
-	srv := server.New(mux, HooliServiceAddr)
-
-	err := srv.ListenAndServeTLS(HooliCertFile, HooliKeyFile)
-	if err != nil {
-		log.Fatalf("server failed to start: %v", err)
-	}
+	fmt.Println("Welcome to hooli!!!")
 }
-
