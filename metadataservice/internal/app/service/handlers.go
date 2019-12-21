770c4e9a26a0923e3b4ba0c4d1d87e56bd922a27
Kevin Travers <ktravers@cbs.com>
2019-12-20 22:07:09 -0500

    Added handlers to service using gorilla

---
+++ metadataservice/internal/app/service/handlers.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func GetAccount(w http.ResponseWriter, r *http.Request) {
+
+	// Read the 'accountId' path parameter from the mux map
+	var accountID = mux.Vars(r)["accountId"]
+
+	// If found, marshal into JSON, write headers and content
+	data, _ := json.Marshal(accountID)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
