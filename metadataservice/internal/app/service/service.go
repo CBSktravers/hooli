f9d006cc6ad2556548b152c1b271f2c95549bba8
Kevin Travers <ktravers@cbs.com>
2019-12-20 23:18:51 -0500

    Adding more functions handlers to service

--- metadataservice/internal/app/service/service.go
+++ metadataservice/internal/app/service/service.go
@@ -6,16 +6,32 @@ import (
 	"time"
 )
 
-const message = "Hello GopherCon UK 2018!"
-
 type Handlers struct {
 	logger *log.Logger
 }
 
-func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
+func (h *Handlers) GetRow(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(message))
+	w.Write([]byte("Here is the row"))
+}
+
+func (h *Handlers) AddRow(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Row added to table"))
+}
+
+func (h *Handlers) UpdateRow(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Row updated"))
+}
+
+func (h *Handlers) DeleteRow(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("row deleted"))
 }
 
 func (h *Handlers) Logger(next http.HandlerFunc) http.HandlerFunc {
@@ -26,7 +42,11 @@ func (h *Handlers) Logger(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 func (h *Handlers) SetupRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("/", h.Logger(h.Home))
+	mux.HandleFunc("/GetRow", h.Logger(h.GetRow))
+	mux.HandleFunc("/AddRow", h.Logger(h.AddRow))
+	mux.HandleFunc("/UpdateRow", h.Logger(h.UpdateRow))
+	mux.HandleFunc("/DeleteRow", h.Logger(h.DeleteRow))
+
 }
 
 func NewHandlers(logger *log.Logger) *Handlers {
