3f55c11169121022d08356fcd4f7c922db746ac8
Kevin Travers <ktravers@cbs.com>
2019-12-20 23:11:48 -0500

    testing standard libary over gorilla

--- metadataservice/internal/app/service/webserver.go
+++ metadataservice/internal/app/service/webserver.go
@@ -8,11 +8,31 @@ import (
 
 //StartWebServer starts a webserver at port
 func StartWebServer(port string) {
+	logger := log.New(os.Stdout, "hooli ", log.LstdFlags|log.Lshortfile)
+	logger.Println("Starting metadata HTTP service at " + port)
+
+	h := NewHandlers(logger)
+
+	mux := http.NewServeMux()
+
+	h.SetupRoutes(mux)
+
+	err := http.ListenAndServe(":"+port, mux) // Goroutine will block here
+
+	if err != nil {
+		logger.Println("An error occured starting HTTP listener at port " + port)
+		logger.Println("Error: " + err.Error())
+	}
+}
+
+//StartWebServer starts a webserver at port
+func StartWebServerOld(port string) {
+	logger := log.New(os.Stdout, "hooli ", log.LstdFlags|log.Lshortfile)
+	logger.Println("Starting metadata HTTP service at " + port)
+
 	r := NewRouter()    // NEW
 	http.Handle("/", r) // NEW
-	logger := log.New(os.Stdout, "hooli ", log.LstdFlags|log.Lshortfile)
 
-	logger.Println("Starting HTTP service at " + port)
 	err := http.ListenAndServe(":"+port, nil) // Goroutine will block here
 
 	if err != nil {
