a91955e02764f70fd6569608540affdb32b86d44
Kevin Travers <ktravers@cbs.com>
2019-12-27 16:16:16 -0500

    misspeeled CreateProfile before again

--- profile/handlers/handlers.go
+++ profile/handlers/handlers.go
@@ -8,4 +8,4 @@ func GetProfile(response http.ResponseWriter, request *http.Request)    {}
 func GetProfiles(response http.ResponseWriter, request *http.Request)   {}
 func UpdateProfile(response http.ResponseWriter, request *http.Request) {}
 func DeleteProfile(response http.ResponseWriter, request *http.Request) {}
-func CreaterPofile(response http.ResponseWriter, request *http.Request) {}
+func CreateProfile(response http.ResponseWriter, request *http.Request) {}
