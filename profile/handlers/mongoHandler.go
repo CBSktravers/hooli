d1691ea3c8585e81f0fa9a7c3fad9eaf43766d8b
Kevin Travers <ktravers@cbs.com>
2020-01-08 13:54:54 -0500

    Create function to create client for mongo connaction

--- profile/handlers/mongoHandler.go
+++ profile/handlers/mongoHandler.go
@@ -10,7 +10,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func createSession() *mongo.Client {
+func createClient() *mongo.Client {
 	// Set client options
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
@@ -32,46 +32,9 @@ func createSession() *mongo.Client {
 	return client
 }
 
-func addProfile() []models.Profile {
+func AddProfileMongo() {
 	var profiles []models.Profile
-	session := createSession()
-	// Get a handle for your collection
-	collection := session.Database("mongodb").Collection("profile")
-	// Some dummy data to add to the Database
-	test := models.Profile{"test_name", "test", map[string]string{
-		"provider_id": "test.com",
-		"provider":    "TEST",
-	}}
-
-	// Insert a single document
-	insertResult, err := collection.InsertOne(context.TODO(), test)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Inserted a single document: ", insertResult.InsertedID)
-
-	return profiles
-}
-
-func AddProfileMong() {
-	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connected to MongoDB!")
+	client := createClient()
 
 	// Get a handle for your collection
 	collection := client.Database("mongodb").Collection("profile")
