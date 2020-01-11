3f7a1081330358ba6305440eb8f5126808b9c025
Kevin Travers <ktravers@cbs.com>
2020-01-11 13:34:37 -0500

    need to pass serivce and logger next to finish repo pattern

--- cmd/hooli/main.go
+++ cmd/hooli/main.go
@@ -1,14 +1,62 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/CBSktravers/hooli/pkg/profile/driver"
+	"github.com/CBSktravers/hooli/pkg/profile"
+	profileRepo "github.com/CBSktravers/hooli/pkg/profile/repository"
+	"github.com/CBSktravers/hooli/pkg/profile/routers"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var appName = "profile service"
 
 func main() {
 	fmt.Printf("Starting %v\n", appName)
-	driver.StartWebServer("8080")
+	StartWebServer("8080")
+}
+
+//StartWebServer starts the profile server
+func StartWebServer(port string) {
+	// Establish client to mongodabase
+	client := createClient()
+
+	// Get a handle for your collection
+	collection := client.Database("mongodb").Collection("profile")
+	profileSvc := profile.NewDefaultService(profileRepo.NewMongo(collection))
+
+	//STOP using gorilla follow orignal to pass logger and service
+	r := routers.NewRouter()
+	http.Handle("/", r)
+	log.Println("Starting HTTP service at " + port)
+	err := http.ListenAndServe(":"+port, nil) // Goroutine will block here
+
+	if err != nil {
+		log.Println("An error occured starting HTTP listener at port " + port)
+		log.Println("Error: " + err.Error())
+	}
+}
+func createClient() *mongo.Client {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB!")
+	return client
 }
